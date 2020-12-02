4f79a3f17bdc3c1bdd19850bdca489ca078ca9d2
dmurayama93 <dylan@gamedevhq.com>
2020-12-02 10:08:45 -1000

    Final Boss basic movement added

--- Assets/Scripts/FinalBoss.cs
+++ Assets/Scripts/FinalBoss.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class FinalBoss : MonoBehaviour
 {
+    
+    //1st attack pattern
+    //Boss turn red and fires charged beam
+    //2nd attack pattern enables at 50% hp
+    //Boss goes to top of the screen, slows down and activates a downward beam while moving backwards
+    //hp
+    [SerializeField] private int _hp = 100;
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private Transform _pointA;
+    [SerializeField] private Transform _pointB;
+    private Transform _curTarget;
+
+    //basic movement
+    //basic fire
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _curTarget = _pointB;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SecondPhaseAbility();
+            Debug.Log("Move");
+        }
+        StandardMovement();
+    }
+    private void BossEntrance()
+    {
+        //entrance pattern
+        //boss moves onto screen and rotates to face the player
+    }
+    private void StandardMovement()
+    {
+        //Boss moves up and down on y axis randomly
+        transform.position = Vector3.MoveTowards(transform.position, _curTarget.position, _speed * Time.deltaTime);
+        if (transform.position == _pointA.position)
+        {
+            _curTarget = _pointB;
+        }
+        if (transform.position == _pointB.position)
+        {
+            _curTarget = _pointA;
+        }
+    }
+    private void SecondPhaseAbility()
+    {
+        _pointB.position = new Vector3(transform.position.x, 15.0f, 0);
+        if (Vector3.Distance(transform.position, _pointB.position) > 1.0f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _pointB.position, _speed * Time.deltaTime);
+        }
         
     }
 }
