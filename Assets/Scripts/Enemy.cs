ecca3956ad4e6a0a3d5c2fc2bac20a08b4a96bff
Kurt Noe <31440372+KurtsCode@users.noreply.github.com>
2020-12-02 15:28:46 -1000

    Updated weapon parts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool exploded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Fireball")
+        {
+            Debug.Log("Hit detected");
+        }
+
+        if(other.tag == "Beam")
+        {
+            Debug.Log("Hit detected");
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject _laserPrefab;
+
+    // Player attack values
+    //[SerializeField]
+    private float _fireRate = 0.25f;
+
+    private float _canFire = -1.0f;
+
+
+    /* Boolean that affects the tracking behavior of the homing shots.
+     * ENABLED: The shots will change their trajectory mid flight to a new valid target if one is detected.
+     * DISABLED: The shots will choose an inital target and will only travel towards that location. 
+     *           If the target was destroyed before reaching the shot reaches it the shot will continue flying along its current trajectory.
+    */
+    public bool _strictTracking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+       
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+
+        Instantiate(_laserPrefab, transform.position + new Vector3(1.05f, 0, 0), Quaternion.identity);
+    }
+
+
+    public void Damage()
+    {
+
+    }
+}
