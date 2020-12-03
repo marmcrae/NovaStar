9ce2fa6c3f7b47eb92c5df786c003b865b0d7eb0
chrisgamedevhq <73717970+chrisgamedevhq@users.noreply.github.com>
2020-12-03 11:09:50 -1000

    added spawn manager

---
+++ Assets/Scripts/Enemy/EnemyGenericMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenericMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1.0f;
+    [SerializeField] float frequency = 1.5f; // Speed of sine movement
+    [SerializeField] float magnitude = 5f; //  Size of sine movement
+    Vector3 pos;
+    Vector3 axis;
+
+    private bool _isAlive = true;
+
+    void Start()
+    {
+        pos = transform.position;
+        axis = transform.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isAlive)
+        {
+            CalculateMove();
+        }
+    }
+
+    private void CalculateMove()
+    {
+        pos += Vector3.left * Time.deltaTime * _speed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+    }
+
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private bool _stopSpawning = false;
+    [SerializeField] private GameObject _enemyContainer;
+
+    // credit to Brackeys start
+    public enum SpawnState { SPAWNING, WAITING, COUNTING }
+
+    // Serialized Fields
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform[] enemy;
+        public int count;
+        public float rate;
+    }
+    public Wave[] waves;
+    public Transform[] spawnPoints;
+    private int nextWave = 0;
+    //end
+
+    private float timeBetweenWaves = 5f;
+    private float waveCountdown;
+    private float searchCountdown = 1f;
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        if (spawnPoints.Length <= 0)
+        {
+            Debug.LogError("No SpawnPoints found");
+        }
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+        if (!_stopSpawning)
+        {
+            if (state == SpawnState.WAITING)
+            {
+                if (!EnemyIsAlive())
+                {
+                    WaveCompleted();
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (waveCountdown <= 0)
+            {
+                if (state != SpawnState.SPAWNING)
+                {
+                    StartCoroutine(SpawnWave(waves[nextWave]));
+                }
+            }
+            else
+            {
+                waveCountdown -= Time.deltaTime;
+            }
+        }
+    }
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("loop");
+        }
+
+        else
+        {
+            nextWave++;
+        }
+    }
+    bool EnemyIsAlive()
+    {
+        searchCountdown -= Time.deltaTime;
+        if (searchCountdown <= 0f)
+        {
+            searchCountdown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        for (int i = 0; i < _wave.count; i++)
+        {
+            SpawnEnemy(_wave.enemy[Random.Range(0, _wave.enemy.Length)]);
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+        state = SpawnState.WAITING;
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(_enemy, _sp.position, _sp.rotation);
+    }
+}
