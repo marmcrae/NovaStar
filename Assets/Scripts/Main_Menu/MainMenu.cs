465054c66b779444bf203cd7e11a3a3616188eb7
Rjyamura <Rjyamura@gmail.com>
2020-12-03 17:10:11 -1000

    Adds Options Menu

---
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _optionsMenu;
+    [SerializeField]
+    private AudioMixer _audioMixer;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1); //scene 1 is main game in current build/may be subject to change
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenuOn()
+    {
+        _optionsMenu.SetActive(true);
+    }
+
+    public void OptionsMenuOff()
+    {
+        _optionsMenu.SetActive(false);
+    }
+
+    public void SetVolume(float volume)
+    {
+        _audioMixer.SetFloat("Volume", volume);
+    }
+
+
+    //start game
+    //quit game
+    //options overlay
+    //set volume
+    //set brightness (post-processing)
+
+}
